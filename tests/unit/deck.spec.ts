3a0e0ff9aff12068ef96387eea4a4fbbaffd20d0
epndavis <47059262+epndavis@users.noreply.github.com>
2020-05-22 17:59:27 +0100

    Add cut test

--- tests/unit/deck.spec.ts
+++ tests/unit/deck.spec.ts
@@ -10,4 +10,12 @@ describe('Deck', () => {
     it('can access deck array', () => {
         expect(_deck.deck).toStrictEqual([1,2,3,4,5])
     })
+
+    it('can cut the deck into two new decks', () => {
+        const decks = _deck.cut();
+
+        expect(decks.length).toBe(2)
+        expect(decks[0]).toBeInstanceOf(Deck)
+        expect(decks[1]).toBeInstanceOf(Deck)
+    })
 })
