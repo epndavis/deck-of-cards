dbca682ef05823276e1b76bb2d4225101ee03439
epndavis <47059262+epndavis@users.noreply.github.com>
2020-05-22 14:13:01 +0100

    Add draw card function

--- src/deck.ts
+++ src/deck.ts
@@ -5,6 +5,17 @@ class Deck {
     constructor (_deck: Array<any>) {
         this.deck = _deck
     }
+
+    // Draw a card from the deck and return it to the user, this simulates
+    // a literal draw from the deck and so the card must be removed from 
+    // the deck array
+    draw(): any | Error {
+        if (this.deck.length == 0) {
+            throw new Error('No more cards in the deck!')
+        }
+
+        return this.deck.shift()
+    }
 }
 
 
