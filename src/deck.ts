d6a609e7694aa488e6a3db15e0a6e3307e9dfcdb
epndavis <47059262+epndavis@users.noreply.github.com>
2020-05-22 14:00:24 +0100

    Add shuffle function: - Shuffle the deck using the Durstenfeld solution to the Fisher-Yates shuffle - return this (Deck class) to allow the user to chain functions

--- src/deck.ts
+++ src/deck.ts
@@ -5,6 +5,20 @@ class Deck {
     constructor (_deck: Array<any>) {
         this.deck = _deck
     }
+
+    // Shuffle the deck using the Durstenfeld solution to the Fisher–Yates shuffle
+    shuffle(): Deck {
+        const deckLength = this.deck.length
+
+        for (let i = deckLength - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+
+            // Use destructuring assignment to swap values
+            [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+        }
+
+        return this
+    }
 }
 
 
