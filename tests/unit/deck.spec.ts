cb6e280733d4f309b2fade52647b0ac0280d0d4c
epndavis <47059262+epndavis@users.noreply.github.com>
2020-05-22 18:06:48 +0100

    Add draw tests

--- tests/unit/deck.spec.ts
+++ tests/unit/deck.spec.ts
@@ -10,4 +10,23 @@ describe('Deck', () => {
     it('can access deck array', () => {
         expect(_deck.deck).toStrictEqual([1,2,3,4,5])
     })
+
+    it('can draw a card from the deck and remove that card from the instance', () => {
+        const card = _deck.draw()
+
+        expect(card).toBe(1)
+        expect(_deck.deck.length).toBe(4)
+    })
+
+    it('throws an error if no cards are left to draw', () => {
+        const deckLength = _deck.deck.length
+        
+        for (let i = 0; i < deckLength; i++) {
+            _deck.draw()
+        }
+
+        expect(() => {
+            _deck.draw()
+        }).toThrow()
+    })
 })
