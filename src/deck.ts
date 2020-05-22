a8718e9ea102b893f7fbe30f9d471b6eb61234e4
epndavis <47059262+epndavis@users.noreply.github.com>
2020-05-22 16:34:42 +0100

    Add cut function

--- src/deck.ts
+++ src/deck.ts
@@ -5,6 +5,17 @@ class Deck {
     constructor (_deck: Array<any>) {
         this.deck = _deck
     }
+
+    // Cut the deck into two new deck classes
+    cut(): [Deck, Deck] {
+        const deckLength = this.deck.length
+        const splitIndex = Math.ceil(deckLength / 2)
+
+        return [
+            new Deck(this.deck.slice(0, splitIndex)),
+            new Deck(this.deck.slice(splitIndex, deckLength))
+        ]
+    }
 }
 
 
